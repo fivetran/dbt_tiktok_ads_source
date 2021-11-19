d378e5d52b00085afe91045540260bc39398b928
fivetran-reneeli <renee.li@fivetran.com>
2021-11-19 15:22:40 -0600

    get column macros

---
+++ macros/get_ad_history_columns.sql
@@ -0,0 +1,33 @@
+{% macro get_ad_history_columns() %}
+
+{% set columns = [
+    {"name": "_fivetran_synced", "datatype": dbt_utils.type_timestamp()},
+    {"name": "ad_id", "datatype": dbt_utils.type_numeric()},
+    {"name": "ad_name", "datatype": dbt_utils.type_string()},
+    {"name": "ad_text", "datatype": dbt_utils.type_string()},
+    {"name": "adgroup_id", "datatype": dbt_utils.type_numeric()},
+    {"name": "advertiser_id", "datatype": dbt_utils.type_numeric()},
+    {"name": "app_name", "datatype": dbt_utils.type_string()},
+    {"name": "call_to_action", "datatype": dbt_utils.type_string()},
+    {"name": "campaign_id", "datatype": dbt_utils.type_numeric()},
+    {"name": "click_tracking_url", "datatype": dbt_utils.type_string()},
+    {"name": "create_time", "datatype": dbt_utils.type_timestamp()},
+    {"name": "display_name", "datatype": dbt_utils.type_string()},
+    {"name": "impression_tracking_url", "datatype": dbt_utils.type_string()},
+    {"name": "is_aco", "datatype": "boolean"},
+    {"name": "is_creative_authorized", "datatype": "boolean"},
+    {"name": "is_new_structure", "datatype": "boolean"},
+    {"name": "landing_page_url", "datatype": dbt_utils.type_string()},
+    {"name": "open_url", "datatype": dbt_utils.type_string()},
+    {"name": "opt_status", "datatype": dbt_utils.type_string()},
+    {"name": "playable_url", "datatype": dbt_utils.type_string()},
+    {"name": "profile_image", "datatype": dbt_utils.type_string()},
+    {"name": "status", "datatype": dbt_utils.type_string()},
+    {"name": "updated_at", "datatype": dbt_utils.type_timestamp()},
+    {"name": "video_id", "datatype": dbt_utils.type_string()}
+
+] %}
+
+{{ return(columns) }}
+
+{% endmacro %}
---
+++ macros/get_adgroup_report_daily_columns.sql
@@ -0,0 +1,51 @@
+{% macro get_adgroup_report_daily_columns() %}
+
+{% set columns = [
+    {"name": "_fivetran_synced", "datatype": dbt_utils.type_timestamp()},
+    {"name": "adgroup_id", "datatype": dbt_utils.type_numeric()},
+    {"name": "average_video_play", "datatype": dbt_utils.type_float()},
+    {"name": "average_video_play_per_user", "datatype": dbt_utils.type_float()},
+    {"name": "clicks", "datatype": dbt_utils.type_numeric()},
+    {"name": "comments", "datatype": dbt_utils.type_numeric()},
+    {"name": "conversion", "datatype": dbt_utils.type_numeric()},
+    {"name": "conversion_rate", "datatype": dbt_utils.type_float()},
+    {"name": "cost_per_1000_reached", "datatype": dbt_utils.type_float()},
+    {"name": "cost_per_conversion", "datatype": dbt_utils.type_float()},
+    {"name": "cost_per_result", "datatype": dbt_utils.type_float()},
+    {"name": "cost_per_secondary_goal_result", "datatype": dbt_utils.type_string()},
+    {"name": "cpc", "datatype": dbt_utils.type_float()},
+    {"name": "cpm", "datatype": dbt_utils.type_float()},
+    {"name": "ctr", "datatype": dbt_utils.type_float()},
+    {"name": "follows", "datatype": dbt_utils.type_numeric()},
+    {"name": "impressions", "datatype": dbt_utils.type_numeric()},
+    {"name": "likes", "datatype": dbt_utils.type_numeric()},
+    {"name": "profile_visits", "datatype": dbt_utils.type_numeric()},
+    {"name": "profile_visits_rate", "datatype": dbt_utils.type_float()},
+    {"name": "reach", "datatype": dbt_utils.type_numeric()},
+    {"name": "real_time_conversion", "datatype": dbt_utils.type_numeric()},
+    {"name": "real_time_conversion_rate", "datatype": dbt_utils.type_float()},
+    {"name": "real_time_cost_per_conversion", "datatype": dbt_utils.type_float()},
+    {"name": "real_time_cost_per_result", "datatype": dbt_utils.type_float()},
+    {"name": "real_time_result", "datatype": dbt_utils.type_numeric()},
+    {"name": "real_time_result_rate", "datatype": dbt_utils.type_float()},
+    {"name": "result", "datatype": dbt_utils.type_numeric()},
+    {"name": "result_rate", "datatype": dbt_utils.type_float()},
+    {"name": "secondary_goal_result", "datatype": dbt_utils.type_string()},
+    {"name": "secondary_goal_result_rate", "datatype": dbt_utils.type_string()},
+    {"name": "shares", "datatype": dbt_utils.type_numeric()},
+    {"name": "skan_conversion", "datatype": dbt_utils.type_numeric()},
+    {"name": "skan_cost_per_conversion", "datatype": dbt_utils.type_float()},
+    {"name": "spend", "datatype": dbt_utils.type_float()},
+    {"name": "stat_time_day", "datatype": dbt_utils.type_timestamp()},
+    {"name": "video_play_actions", "datatype": dbt_utils.type_numeric()},
+    {"name": "video_views_p_100", "datatype": dbt_utils.type_numeric()},
+    {"name": "video_views_p_25", "datatype": dbt_utils.type_numeric()},
+    {"name": "video_views_p_50", "datatype": dbt_utils.type_numeric()},
+    {"name": "video_views_p_75", "datatype": dbt_utils.type_numeric()},
+    {"name": "video_watched_2_s", "datatype": dbt_utils.type_numeric()},
+    {"name": "video_watched_6_s", "datatype": dbt_utils.type_numeric()}
+] %}
+
+{{ return(columns) }}
+
+{% endmacro %}
