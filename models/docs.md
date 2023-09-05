43da8ec3beffc15e303d744498aa92c1d0cac3f3
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-09-05 17:56:07 -0500

    regen docs

---
+++ models/docs.md
@@ -0,0 +1,4 @@
+
+{% docs source_relation %}
+The source of the record if the unioning functionality is being used. If not this field will be empty.
+{% enddocs %}
--- models/tmp/stg_tiktok_ads__ad_group_report_hourly_tmp.sql
+++ models/tmp/stg_tiktok_ads__ad_group_report_hourly_tmp.sql
@@ -1,15 +1,13 @@
-ADD source_relation WHERE NEEDED + CHECK JOINS AND WINDOW FUNCTIONS! (Delete this line when done.)
-
 {{ config(enabled=var('ad_reporting__tiktok_ads_enabled', true)) }}
 
 {{
     fivetran_utils.union_data(
-        table_identifier='ad_group_report_hourly', 
+        table_identifier='adgroup_report_hourly', 
         database_variable='tiktok_ads_database', 
         schema_variable='tiktok_ads_schema', 
         default_database=target.database,
         default_schema='tiktok_ads',
-        default_variable='ad_group_report_hourly_source',
+        default_variable='adgroup_report_hourly_source',
         union_schema_variable='tiktok_ads_union_schemas',
         union_database_variable='tiktok_ads_union_databases'
     )
